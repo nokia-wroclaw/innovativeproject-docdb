9ea952239be74db29f08645844b41c0fdbb8747d
Dasiuss <Dasiuss@StarHost>
2014-11-06 00:03:26 +0100

    updated Application class handling websocket returning js SearchScript creating elastic search server and search manager

--- DocDB/app/controllers/Application.java
+++ DocDB/app/controllers/Application.java
@@ -3,28 +3,35 @@ package controllers;
 import java.io.File;
 import java.io.IOException;
 
+import model.ClientWebSocket;
 import model.ElasticSearchManager;
 import model.ElasticSearchServer;
 import model.FileParser;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.io.Files;
 
 import play.*;
+import play.libs.Akka;
 import play.mvc.*;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import views.html.*;
+import views.js.SearchScript;
 
 public class Application extends Controller {
 
     private static String dirPath = "files/";
-    private static FileParser fp = new FileParser();
     private static ElasticSearchServer elasticServer = new ElasticSearchServer();
-    private static ElasticSearchManager elasticSearch = new ElasticSearchManager();    
+    private static ElasticSearchManager elasticSearch = new ElasticSearchManager(); 
+    private static FileParser fp = new FileParser();   
     
 	public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+	
     
     public static Result upload(){
     	MultipartFormData body = request().body().asMultipartFormData();
@@ -45,7 +52,7 @@ public class Application extends Controller {
 				e.printStackTrace();
 			}
 
-//			fp.parseFile(file);
+			fp.parseFile(file);
 
 			return ok(index.render("Plik "+fileName+" przesłano na serwer"));
 		} else {
@@ -54,4 +61,25 @@ public class Application extends Controller {
 		}
     }
 
+    
+
+	//websocket
+
+
+	public static Result SearchScript() {
+		return ok(SearchScript.render());
+	}
+	
+	public static WebSocket<JsonNode> WebSocket() {
+		return new WebSocket<JsonNode>() {
+
+			String user = session("connected");
+			public void onReady(WebSocket.In<JsonNode> in, WebSocket.Out<JsonNode> out) {
+
+				//mamy juz in i out
+				Akka.system().actorOf(Props.create(ClientWebSocket.class, in, out, user, elasticSearch, elasticServer));
+			}
+
+		};
+	}
 }

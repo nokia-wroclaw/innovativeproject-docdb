7bac5b097dee0e817dfa0310a2ed14bc29e24c26
Dasiuss <Dasiuss@StarHost>
2014-11-07 11:57:44 +0100

    FileHandler update

--- DocDB/app/model/FileHandler.java
+++ DocDB/app/model/FileHandler.java
@@ -1,11 +1,17 @@
 package model;
 
 import java.io.File;
+import java.io.IOException;
+
+import com.google.common.io.Files;
+
+import play.Logger;
 
 public class FileHandler {
 
 	private FileParser fileParser;
 	private ElasticSearchManager esm;
+    private static String dirPath = "files/";
 	
 	public FileHandler() {
 		fileParser = new FileParser();
@@ -13,7 +19,19 @@ public class FileHandler {
 	}
 	
 	public void handleFile(File file) {
-		String[] parsedFile = fileParser.parseFile(file);
+		
+		String newPath=dirPath + file.getName();
+		File newFile = new File(newPath);
+		
+		try {
+			Files.move(file, newFile);
+			Logger.info("file saved in:"+newPath);
+		} catch (IOException e) {
+			Logger.info("file save failed");
+			e.printStackTrace();
+		}
+		
+		String[] parsedFile = fileParser.parseFile(newFile);
 		if (parsedFile!=null){
 			esm.putJsonDocument(parsedFile);
 		}
